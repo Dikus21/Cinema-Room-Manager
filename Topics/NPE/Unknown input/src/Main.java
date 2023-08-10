3c4dffae58a0bdd9eb81aebcf830bf11cbe075a2
Dikus21 <72666412+Dikus21@users.noreply.github.com>
2023-08-10 16:16:52 +0700

    Delete Topics directory

--- Topics/NPE/Unknown input/src/Main.java
+++
@@ -1,7 +0,0 @@
-class Util {
-    // correct this method to avoid NPE
-    public static void printLength(String name) {
-        int size = name == null ? 0 : name.length();
-        System.out.println(size);
-    }
-}
